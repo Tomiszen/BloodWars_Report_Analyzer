67fc24fb6274b45b0f9b184b0dff53ad6065fe63
Tomiszen <tomisz@onet.eu>
2024-10-09 23:10:26 +0200

    1. Create Player class 2. Read players names

--- model.py
+++ model.py
@@ -16,4 +16,32 @@ class Report:
         self.defender = defender
 
 
+class Player:
 
+    name: str
+    players_list = []
+
+    def __init__(self, player_id, name, clan):
+        self.id = player_id
+        self.name = name.replace("(@)", "").replace("(*)", "").strip()
+        self.clan = clan
+        type(self).players_list.append(self)
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return self.name
+
+    @classmethod
+    def get_player(cls, player_id=None, name=None):
+        if player_id:
+            return list(filter(lambda elem: elem.id == player_id, cls.players_list))[0]
+        elif name:
+            return list(filter(lambda elem: elem.name == name, cls.players_list))[0]
+        else:
+            return None
+
+    @classmethod
+    def get_clan_players(cls, clan):
+        return list(filter(lambda elem: elem.clan == clan, cls.players_list))
--- service.py
+++ service.py
@@ -1,6 +1,7 @@
 import model
 import requests
 from bs4 import BeautifulSoup
+import re
 
 
 def create_report_object(url, report_type):
@@ -23,3 +24,21 @@ def make_soup(report):
 def get_opponents(soup, report):
     report.set_attacker(soup.find('td', {'class': 'attacker'}).text)
     report.set_defender(soup.find('td', {'class': 'defender'}).text)
+
+
+def get_players(soup, side, clan):
+    if side == 'attacker':
+        players_soup = soup.find('div', {'class': 'amblist rlr fll'})
+    else:
+        players_soup = soup.find('div', {'class': 'amblist rll flr'})
+    players_soup = players_soup.find_all('span')
+    read_players(players_soup, clan)
+
+
+def read_players(soup, clan):
+    for player in soup:
+        player_id = re.findall("[a-z]{2}_[0-9]+", str(player))[0]
+        name = player.text.strip()
+        model.Player(player_id, name, clan)
+
+
